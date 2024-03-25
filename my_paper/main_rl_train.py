29cd2d42f90497402a16cf84cf691e3dd34fbd3c
Qserialport <2664907853@qq.com>
2024-03-25 17:15:28 +0800

    3.25_2

--- my_paper/main_rl_train.py
+++ my_paper/main_rl_train.py
@@ -3,8 +3,10 @@ import gym
 import numpy as np
 import torch
 import torch.nn as nn
+from agent import Agent
 
 env = gym.make('CartPole-v1')
+s = env.reset() #初始化并输出一系列观测值
 
 TARGET_UPDATE_FREQUENCY = 10    #每十局更新一次Q网络参数
 
@@ -14,6 +16,11 @@ EPSILON_END = 0.02         #贪心算法参数会逐渐减小
 
 n_episode = 5000   #一共玩5000局
 n_time_step = 1000  #每一局有1000步
+
+n_state = len(s)
+n_action = env.action_space.n
+
+agent = Agent(n_input=n_state, n_output=n_action)
 
 REWARD_BUFFER = np.empty(shape=n_episode)
 for episode_i in range(n_episode):
@@ -25,7 +32,7 @@ for episode_i in range(n_episode):
         if random_sample <= epsilon:            #根据epsilon选择将要执行的动作
             a = env.action_space.sample()
         else:
-            a = agent.online_net.act()  # TODO
+            a = agent.online_net.act(s)  # TODO
 
         s_, r, done, info = env.step(a)     #执行动作a，并观测下一状态、奖励、是否结束
         agent.memo.add_memo(s, a, r, done, s_)  # TODO  ,加入经验池
@@ -37,7 +44,7 @@ for episode_i in range(n_episode):
             REWARD_BUFFER[episode_i] = episode_reward
             break
 
-        batch_s，batch_a, batch_r, batch_done, batch_s_ = agent.memo.sample()   #TODO   #每个batch由四元组组成（s,r,a,s_）
+        batch_s, batch_a, batch_r, batch_done, batch_s_ = agent.memo.sample()   #TODO   #每个batch由四元组组成（s,r,a,s_）
 
         # Compute target using td_AL
         target_q_values = agent.target_net(batch_s_)    #神经网络输出为一个张量，注意使用的是下一时刻的状态
@@ -52,12 +59,12 @@ for episode_i in range(n_episode):
         loss = nn.functional.smooth_l1_loss(targets, a_q_values)
 
         # Gradient descent
-        agent.optimizer.zero_grad()   #TODO
+        agent.optimizer.zero_grad()   #TODO 要输入状态
         loss.backward() #反向传播
         agent.optimizer.step()  #至此完成神经网络的一次梯度下降   #TODO
 
     if episode_i % TARGET_UPDATE_FREQUENCY == 0:
-        agent.target_net.load_state_dict(agent.online_net.dict())   #TODO
+        agent.target_net.load_state_dict(agent.online_net.state_dict())   #TODO
 
         # Show the training process
         print("Episode: {}".format(episode_i))
